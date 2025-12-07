6860397aa87233c99c6df55f3a60f1de115498fa
neonew <>
2025-12-07 15:25:22 +0100

    Use button interrupts

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use core::num::NonZero;
 use esp_idf_hal::delay::FreeRtos;
 use esp_idf_hal::gpio::*;
 use esp_idf_hal::peripherals::Peripherals;
+use esp_idf_hal::task::notification::Notification;
 
 fn main() -> anyhow::Result<()> {
   // It is necessary to call this function once. Otherwise, some patches to the runtime
@@ -15,21 +17,47 @@ fn main() -> anyhow::Result<()> {
   let peripherals = Peripherals::take()?;
 
   // See here:
-  // https://github.com/esp-rs/esp-idf-hal/blob/v0.45.2/examples/button.rs
-  let mut green_led = PinDriver::output(peripherals.pins.gpio45)?;
-  let mut red_led = PinDriver::output(peripherals.pins.gpio42)?;
+  // https://github.com/esp-rs/esp-idf-hal/blob/v0.45.2/examples/button_interrupt.rs
+  let mut green_led = PinDriver::output(peripherals.pins.gpio42)?;
+  let mut red_led = PinDriver::output(peripherals.pins.gpio45)?;
   let mut button = PinDriver::input(peripherals.pins.gpio4)?;
   button.set_pull(Pull::Up)?;
+  button.set_interrupt_type(InterruptType::PosEdge)?;
+
+  let mut led_state = false;
+  // Turn both LEDs off
+  red_led.set_high()?;
+  green_led.set_high()?;
 
   loop {
-    FreeRtos::delay_ms(10);
+    // prepare communication channel
+    let notification = Notification::new();
+    let waker = notification.notifier();
 
-    if button.is_high() {
-      red_led.set_high()?;
-      green_led.set_low()?;
-    } else {
-      red_led.set_low()?;
-      green_led.set_high()?;
+    // register interrupt callback, here it's a closure on stack
+    unsafe {
+      button
+        .subscribe_nonstatic(move || {
+          waker.notify(NonZero::new(1).unwrap());
+        })
+        .unwrap();
     }
+
+    // enable interrupt, will be automatically disabled after being triggered
+    button.enable_interrupt()?;
+    // block until notified
+    notification.wait_any();
+
+    // toggle the LED
+    if led_state {
+      green_led.set_high()?;
+      led_state = false;
+    } else {
+      green_led.set_low()?;
+      led_state = true;
+    }
+
+    // debounce
+    FreeRtos::delay_ms(200);
   }
 }
